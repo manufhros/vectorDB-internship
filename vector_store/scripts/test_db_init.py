ec9bea8edcbb10595d23896e58cdb1903ea4cf6f
manufhros <manuelfhros@gmail.com>
2025-04-13 12:19:36 +0200

    New pyproject.toml setup

--- vector_store/scripts/test_db_init.py
+++ vector_store/scripts/test_db_init.py
@@ -1,12 +1,10 @@
 # scripts/test_db_init.py
 
 from sqlalchemy import inspect
+
 from vector_store.app.db.database import Base, engine
 
 # 👇 IMPORTA TUS MODELOS EXPLÍCITAMENTE
-from vector_store.app.db.models.library import Library
-from vector_store.app.db.models.document import Document
-from vector_store.app.db.models.chunk import Chunk
 
 print("📦 Creando tablas...")
 Base.metadata.create_all(bind=engine)
